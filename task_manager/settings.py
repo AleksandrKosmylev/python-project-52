a88aa280cb0f245c5f58dfddc68a1df0882be85d
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-01-21 13:24:29 +0300

    raname in apps task_manager.apps.RenderConfig

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -36,7 +36,6 @@ if RENDER_EXTERNAL_HOSTNAME:
 # Application definition
 
 INSTALLED_APPS = [
-    'task_manager.apps.RenderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
