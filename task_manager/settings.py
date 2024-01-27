213513da304f9a119c4ba34d30918b98218fa424
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-01-27 13:19:58 +0300

    whitenoise add

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
     'task_manager',
     'django_bootstrap5',
@@ -131,12 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
