a350bf90561b09f7c8e03b6966a3117b369aa93a
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-01-28 18:23:22 +0300

    admin custom settings and migration

--- task_manager/users/admin.py
+++ task_manager/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+
+from .models import Users
 
 # Register your models here.
+
+
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'full_name','timestamp') # Перечисляем поля, отображаемые в таблице списка статей
+    search_fields = ['user_name', 'full_name']
+    list_filter = (('timestamp', DateFieldListFilter),)
--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    user_name = models.CharField('user_name', max_length=255)
+    full_name = models.CharField('full_name', max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user_name
