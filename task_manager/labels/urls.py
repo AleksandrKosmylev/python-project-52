4f414c14af4b692eb67d0d2495106ccd5a4f8439
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-03-29 16:37:19 +0300

    update labels

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from task_manager.labels.views import main_labels, LabelCreateView, LabelUpdateView, LabelDeleteView
+from task_manager.labels.views import LabelView, LabelCreateView, LabelUpdateView, LabelDeleteView
 
 urlpatterns = [
-    path('', main_labels, name='labels'),
+    path('',LabelView.as_view(), name='labels'),
     path('create/', LabelCreateView.as_view(), name='label_create'),
-    path('<int:id>/update/', LabelUpdateView.as_view(), name='label_update'),
-    path('<int:id>/delete/', LabelDeleteView.as_view(), name='label_delete'),
+    path('<int:pk>/update/', LabelUpdateView.as_view(), name='label_update'),
+    path('<int:pk>/delete/', LabelDeleteView.as_view(), name='label_delete'),
 ]
--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -4,13 +4,34 @@ from task_manager.labels.models import Labels
 from django.views import View
 from task_manager.labels.forms import LabelsForm
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.messages.views import SuccessMessageMixin
+from task_manager.mixins import CustomLoginRequiredMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
 
+class LabelView(ListView):
+    model = Labels
+    template_name = 'labels/labels.html'
+
+"""
 def main_labels(request):
     labels = Labels.objects.all().order_by('id')
     return render(request, 'labels/labels.html', {'labels': labels})
-
-
+"""
+class LabelCreateView(SuccessMessageMixin, CreateView):
+    model = Labels
+    form_class = LabelsForm
+    template_name = 'form.html'
+    extra_context = {
+        'title': _('Create Label'),
+        'btn_text': _('Create'),
+        'btn_class': 'btn-primary'}
+    success_url = reverse_lazy('labels')
+    success_message = _('Label successfully created!')
+    
+"""
 class LabelCreateView(View):
 
     def get(self, request, *args, **kwargs):
@@ -22,7 +43,20 @@ class LabelCreateView(View):
             label_form.save()
             messages.success(request, 'Метка успешно создана')
             return redirect('labels')
+"""
+class LabelUpdateView(CustomLoginRequiredMixin,SuccessMessageMixin, UpdateView):
+    model = Labels
+    form_class = LabelsForm
+    template_name = 'form.html'
+    extra_context = {
+        'title': _('Update label'),
+        'btn_text': _('Update'),
+        'btn_class': 'btn-primary'}
+    success_url = reverse_lazy('labels')
+    success_message = _('Label successfully update!')
 
+
+"""
 class LabelUpdateView(View):
 
     def get(self, request, **kwargs):
@@ -42,8 +76,20 @@ class LabelUpdateView(View):
             print(form.errors.as_data(), 'errors')
             return HttpResponse("Error label")
         return render(request, 'labels/label_update.html',{'form': form})
+"""
+class LabelDeleteView(CustomLoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Labels
+    extra_context = {
+        'description': _('Are you sure you want to delete'),
+        'title': _('Delete label'),
+        'btn_text': _('Yes, delete'),
+        'btn_class': 'btn-danger'}
+    template_name = 'form.html'
+    # template_name = 'users/user_delete.html'
+    success_url = reverse_lazy('labels')
+    success_message = _("Label successfully deleted!")
 
-
+"""
 class LabelDeleteView(View):
 
     def get(self, request, **kwargs):
@@ -60,3 +106,4 @@ class LabelDeleteView(View):
             return redirect('labels')
         return HttpResponse("DELETE LABEL FAILED. CREATE FLASH FOR IT")
 
+"""
