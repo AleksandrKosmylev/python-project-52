e514efee5b61ee60ac317196922a919d8f579fae
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-04-10 20:54:42 +0300

    change field name in all models and fix on_delete actions

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class Labels(models.Model):
-    name = models.CharField('name', max_length=255)
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
     timestamp = models.DateTimeField("time_stamp", auto_now_add=True)
 
     def __str__(self):
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -2,25 +2,31 @@ from django.db import models
 from task_manager.users.models import CustomUser
 from task_manager.statuses.models import Status
 from task_manager.labels.models import Labels
+from django.utils.translation import gettext as _
 
 
 class Task(models.Model):
-    name = models.CharField('name', max_length=255, unique=True)
-    description = models.TextField('description', blank=True)
+    name = models.CharField(max_length=255, unique=True, verbose_name=_('Name'))
+    description = models.TextField(blank=True, verbose_name=_('Description'))
     author = models.ForeignKey(CustomUser,
-                               on_delete=models.CASCADE,
-                               related_name='author')
+                               on_delete=models.PROTECT,
+                               related_name='task_author',
+                               verbose_name=_('Author'))
     timestamp = models.DateTimeField("time_stamp",
                                      auto_now_add=True)
     executor = models.ForeignKey(CustomUser,
-                                 on_delete=models.CASCADE)
+                                 on_delete=models.PROTECT,
+                                 blank=True,
+                                 null=True,
+                                 related_name='task_executor',
+                                 verbose_name=_('Executor'))
     status = models.ForeignKey(Status,
-                               models.SET_NULL,
-                               blank=True,
-                               null=True)
+                               on_delete=models.PROTECT,
+                               verbose_name=_('Status'))
     labels = models.ManyToManyField(Labels,
+                                    blank=True,
                                     through="IntermediateModel",
-                                    verbose_name='labels')
+                                    verbose_name=_('Labels'))
 
     def __str__(self):
         return '{} {} {} {} {} {}'.format(self.author,
