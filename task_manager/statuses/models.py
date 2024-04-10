83090d4bdbea7b036b86854333b3297ac19504d8
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-04-10 20:34:47 +0300

    change field name in statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext as _
 
 
 class Status(models.Model):
-    name = models.CharField('status', max_length=255)
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
     timestamp = models.DateTimeField("time_stamp", auto_now_add=True)
 
     def __str__(self):
