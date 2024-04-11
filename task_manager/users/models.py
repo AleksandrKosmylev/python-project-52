3129ae4e7497d837450ec16b713077fa32c6ae88
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-04-11 19:14:50 +0300

    change executor from username to fullname

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -6,9 +6,13 @@ class CustomUser(AbstractUser):
 
     class Meta:
         ordering = ["id"]
-
+    """
     def __str__(self):
         return self.username
+    """
+
+    def __str__(self):
+        return self.get_full_name()
 
     def get_absolute_url(self):
         return reverse('users:index')
