b7b0ccc84c635ae6e0b1f0a6c9daae4e9d6b8bff
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-04-17 11:57:53 +0300

    move coverage and flake8 dependencies to group dev. fix rollbar

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from task_manager.users.views import (IndexView,
                                       UserUpdateView,
                                       UserDeleteView,
-                                      UserCreateView,
-                                      index)
+                                      UserCreateView
+                                      )
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='users_index'),
     path('create/', UserCreateView.as_view(), name='user_create'),
     path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
-    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
-    path('rollbar/', index, name='rollbar')
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete')
 ]
